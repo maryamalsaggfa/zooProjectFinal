d784d5e3e500c30270c870d4ec91de89a9bd1418
maryamalsaggfa <maryam1xsa@gmail.com>
2023-11-25 23:50:59 -0800

    Initial Commit

---
+++ zooProjectFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zooProjectFinal
+//
+//  Created by maryam on 11/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zooProjectFinal/zooProjectFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  zooProjectFinalApp.swift
+//  zooProjectFinal
+//
+//  Created by maryam on 11/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct zooProjectFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
