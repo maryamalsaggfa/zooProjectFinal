2110844687e4b83e731a6365b26a168217332a99
maryamalsaggfa <maryam1xsa@gmail.com>
2023-11-29 00:12:51 -0800

    sign up sucess and added the pages

---
+++ zooProjectFinal/LionAR.swift
@@ -0,0 +1,18 @@
+//
+//  LionAR.swift
+//  zooProjectFinal
+//
+//  Created by maryam on 15/05/1445 AH.
+//
+
+import SwiftUI
+
+struct LionAR: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LionAR()
+}
---
+++ zooProjectFinal/sendInvation.swift
@@ -0,0 +1,18 @@
+//
+//  sendInvation.swift
+//  zooProjectFinal
+//
+//  Created by maryam on 15/05/1445 AH.
+//
+
+import SwiftUI
+
+struct sendInvation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sendInvation()
+}
