987239eacd5b239d5e501b188d5fe8d614cdc243
maryamalsaggfa <maryam1xsa@gmail.com>
2023-12-04 22:44:51 -0800

    maryam -

---
+++ zooProjectFinal/UserListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserListView.swift
+//  zooProjectFinal
+//
+//  Created by maryam on 20/05/1445 AH.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserListView()
+}
